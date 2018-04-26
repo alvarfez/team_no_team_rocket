71fc6ece230d82869690f626a8ffe33a15929945
alvarfez <alvar.fernandez@opendeusto.es>
2018-04-26 09:40:06 +0200

    Util 2.0

--- team_no_team_rocket/Util.java
+++ team_no_team_rocket/Util.java
@@ -3,12 +3,41 @@ package team_no_team_rocket;
 public class Util {
 	
 	
-	/** Metodo que comprueba si el numero de caracteres del telefono es correcto y en caso de
-	 * ser null  
-	 * 
+	/** Metodo que comprueba si el numero de caracteres del telefono es correcto y devuelve 
+	 * true. En caso de ser incorrecto devuelve False y en caso de ser null lo completa 
+	 * aleatoriamente como nosotros queremos.
+	 * @param telefono 
 	 */
-	public static void LimitarCaracteres(){
+	public static boolean EntradaTlfno(Integer telefono){
+		if(telefono == null){
+			//TODO generar lista bares para ir sumando +1
+			return false;
+		}
+		
+		else{
+			
+			if(telefono.toString().length() != 9){
+				System.out.println("El numero de telefono no es correcto. Debe tener 9 dígitos");
+				return false;
+			}
+			
+			else{
+				System.out.println("Dato introducido correctamente");
+				return true;
+			}
+		}
+		
 		
 	}
-
+	public static void main(String[] args) {
+		 int telefono = 645456789;
+		 boolean es = EntradaTlfno(telefono);
+		 if(es){
+			 System.out.println("Funciona");
+		 }
+		 else{
+			 System.out.println("No funciona");
+		 }
+		 
+	}
 }
