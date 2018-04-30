1fc7cf95c99e2c9e57a209799d72c1f13ea4470f
alvarfez <alvar.fernandez@opendeusto.es>
2018-04-30 15:29:09 +0200

    util version buena (por ahora)

--- team_no_team_rocket/Util.java
+++ team_no_team_rocket/Util.java
@@ -3,13 +3,29 @@ package team_no_team_rocket;
 public class Util {
 	
 	
-	/** Metodo que comprueba si el numero de caracteres del telefono es correcto y en caso de
-	 * ser null  
-	 * 
+	/** Metodo que comprueba si el numero de caracteres del telefono es correcto y devuelve 
+	 * true. En caso de ser incorrecto devuelve False y en caso de ser null lo completa 
+	 * aleatoriamente como nosotros queremos.
+	 * @param telefono 
 	 */
-	public static void LimitarCaracteres(){
-		
-	}
+	public static boolean EntradaTlfno(Integer telefono){
+		if(telefono == null){
+			//TODO generar lista bares para ir sumando +1
+			return false;
+		}			
+		else{				
+			if(telefono.toString().length() != 9){
+				System.out.println("El numero de telefono no es correcto. Debe tener 9 dígitos");
+				return false;
+			}				
+			else{
+				System.out.println("Dato introducido correctamente");
+				return true;
+				}
+			}
+			
+	 		
+	 	}
 
 	/** MÉTODO BASADO EN CÓDIGO DE WEB: https://medium.com/@manuelmato/c%C3%B3mo-validar-un-dni-en-java-6d7ce7d764aa
 	 * Entrada robusta para DNIs, devuelve true el dni introducido tiene una letra y 8 numeros, false si no
